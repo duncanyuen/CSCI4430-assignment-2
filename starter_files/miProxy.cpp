a8963cee67203cd93d49d0fc40a0f401f8a7ad08
Vincy <77875361+CDsasaku@users.noreply.github.com>
2023-03-18 21:47:00 +0800

    final

--- starter_files/miProxy.cpp
+++ starter_files/miProxy.cpp
@@ -6,7 +6,6 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <vector>
 #include <unordered_map>
 #include <fstream>
@@ -22,7 +21,6 @@
 #include <sys/time.h>
 
 #define BACKLOG 10
-#define MAXPACKETSIZE 65536
 
 using std::cout;
 using std::endl;
@@ -33,18 +31,37 @@ using std::to_string;
 using std::unordered_map;
 using std::vector;
 
-/* Extract the video chunk name from request */
-string get_chunkname(string request)
+// Get client ip address
+string getClientIp(int fd)
+{
+    char client_ip[INET_ADDRSTRLEN];
+    struct sockaddr_in client_addr;
+    int client_len = sizeof(client_addr);
+    getpeername(fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
+    inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+    return (string)client_ip;
+}
+
+// Get the Chunkname
+string getChunkName(string request)
 {
     int get_index = request.find("GET");
     int end_index = request.find(' ', get_index + 4);
     int vod_index = request.rfind("vod", end_index);
-    string fn = request.substr(vod_index + 4, end_index - vod_index - 4);
-    return fn;
+    return request.substr(vod_index + 4, end_index - vod_index - 4);
 }
 
-/* Check if the str is the video data format */
-bool check_video_data(string str)
+// Get Response With New Chunkname which have the adaptive bitrate
+string getResponseWithNewChunkName(string request, string newChunkName)
+{
+    int newChunkStartPos = request.find("GET") + 4;
+    int newChunkEndPos = request.find(' ', newChunkStartPos);
+    newChunkStartPos = request.rfind("vod", newChunkEndPos);
+    return request.replace(newChunkStartPos + 4, newChunkEndPos - newChunkStartPos - 4, newChunkName);
+}
+
+// Check whether is the video
+bool checkIsVideo(string str)
 {
     if (str.find("Seg") == -1)
         return false;
@@ -55,8 +72,8 @@ bool check_video_data(string str)
     return false;
 }
 
-/* Get the value from str based on the key */
-string get_value(string str, string key)
+// Get the value based on the key
+string getValue(string str, string key)
 {
     int key_index = str.find(key);
     int space_index = str.find(' ', key_index);
@@ -64,12 +81,12 @@ string get_value(string str, string key)
     return str.substr(space_index + 1, end - space_index - 1);
 }
 
-/* Recerive the response from the server */
-string recv_response(int server_sd)
+// Receive the header response
+string receiveResponseHeader(int server_sd)
 {
-    string data = "";
+    string response = "";
     char buf;
-    while (1)
+    while (true)
     {
         int bytesRecvd = recv(server_sd, &buf, 1, 0);
 
@@ -79,14 +96,14 @@ string recv_response(int server_sd)
             exit(-1);
         }
 
-        data += buf;
-        if (data.size() >= 4)
+        response += buf;
+        if (response.size() >= 4)
         {
-            if (data.substr(data.size() - 4) == "\r\n\r\n")
+            if (response.substr(response.size() - 4) == "\r\n\r\n")
                 break;
         }
     }
-    return data;
+    return response;
 }
 
 int main(int argc, char const *argv[])
@@ -96,24 +113,30 @@ int main(int argc, char const *argv[])
     float alpha;
     char *log_path;
 
-    if (argc != 6)
+    if (argc == 6)
     {
-        cout << "Usage: ./miProxy --nodns <listen-port> <www-ip> <alpha> <log>" << endl;
-    }
-    else
-    {
-        // normal mode
         listen_port = atoi(argv[2]);
         www_ip = (char *)argv[3];
         alpha = atof(argv[4]);
         log_path = (char *)argv[5];
     }
+    else if (argc == 5)
+    {
+        listen_port = atoi(argv[1]);
+        www_ip = (char *)argv[2];
+        alpha = atof(argv[3]);
+        log_path = (char *)argv[4];
+    }
+    else
+    {
+        cout << "Usage: ./miProxy [--nodns] <listen-port> <www-ip> <alpha> <log>" << endl;
+    }
 
-    // configure port to listen on
+    // Configure port to listen on
     int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (socket_fd < 0)
     {
-        exit(1);
+        return (-1);
     }
 
     struct sockaddr_in proxy_addr;
@@ -125,32 +148,28 @@ int main(int argc, char const *argv[])
     if (retv < 0)
     {
         perror("Error binding stream socket");
-        exit(1);
+        return (-1);
     }
 
     retv = listen(socket_fd, BACKLOG);
     if (retv < 0)
     {
-        exit(1);
+        return (-1);
     }
 
     fd_set read_fd_set;
     vector<int> fds;
     vector<int> bitrates;
-    // stores the throughput for each "CDN"
-    unordered_map<int, double> throughput_map;
-    string f4m;
-    string swf;
-    char client_ip[INET_ADDRSTRLEN];
+    unordered_map<int, double> throughput;
+    string client_ip;
 
-    ofstream log_out;
-    log_out.open(log_path);
+    ofstream logfile;
+    logfile.open(log_path);
 
     while (true)
     {
-        //  why find max fd???
         FD_ZERO(&read_fd_set);
-        FD_SET(socket_fd, &read_fd_set); // add socket_fd to read_fd_set
+        FD_SET(socket_fd, &read_fd_set);
         for (int i = 0; i < (int)fds.size(); i++)
         {
             FD_SET(fds[i], &read_fd_set);
@@ -162,32 +181,27 @@ int main(int argc, char const *argv[])
         }
         max_fd = max(max_fd, socket_fd);
 
-        // maxfd + 1 is important
-        // why?
         retv = select(max_fd + 1, &read_fd_set, NULL, NULL, NULL);
         if (retv < 0)
         {
             perror("Error selecting");
-            exit(1);
+            return (-1);
         }
 
         if (FD_ISSET(socket_fd, &read_fd_set))
         {
-            // socket_fd is part of read_fd_set
             int clientsd = accept(socket_fd, NULL, NULL);
             if (clientsd < 0)
             {
                 perror("Error accepting connection");
-                exit(1);
+                return (-1);
             }
             else
             {
-                // append clientsd to fds
                 fds.push_back(clientsd);
             }
         }
 
-        // copy first, ask questions later
         for (int i = 0; i < (int)fds.size(); ++i)
         {
             if (FD_ISSET(fds[i], &read_fd_set))
@@ -197,31 +211,26 @@ int main(int argc, char const *argv[])
                 bool closed = false;
                 struct timeval t1, t2;
                 int cur_fd = fds[i];
+                client_ip = getClientIp(fds[i]);
 
-                // get client ip address
-                struct sockaddr_in client_addr;
-                int client_len = sizeof(client_addr);
-                getpeername(fds[i], (struct sockaddr *)&client_addr, (socklen_t *)&client_len);
-                inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
-
-                while (1)
+                while (true)
                 {
-                    // receiving the request from clients
+                    // Receiving the request from clients
                     int bytesRecvd = recv(cur_fd, &buf, 1, 0);
 
                     if (bytesRecvd < 0)
                     {
                         perror("Error recving bytes");
                         close(socket_fd);
-                        exit(1);
+                        return (-1);
                     }
                     else if (bytesRecvd == 0) // end of recv
                     {
                         perror("Connection closed");
                         if (fds.size() != 0)
                             fds.erase(fds.begin() + i);
-                        if (throughput_map.find(cur_fd) != throughput_map.end())
-                            throughput_map.erase(cur_fd);
+                        if (throughput.find(cur_fd) != throughput.end())
+                            throughput.erase(cur_fd);
                         closed = true;
                         break;
                     }
@@ -234,27 +243,26 @@ int main(int argc, char const *argv[])
                     }
                 }
 
-                /* Client Keep alive */
                 if (closed == false)
                 {
-                    bool is_video_data = check_video_data(request);
+                    bool isVideo = checkIsVideo(request);
                     int currentBitrate;
                     string chunkName;
 
-                    /* Client is requesting video data,
-                       modify the request for bitrate adaption */
-                    if (is_video_data)
+                    // get the correct chunkname if the received data is video
+                    if (isVideo)
                     {
-                        chunkName = get_chunkname(request);
+                        chunkName = getChunkName(request);
                         int seg_index = chunkName.find("Seg");
                         int bitrate = atoi(chunkName.substr(0, seg_index).c_str());
                         currentBitrate = bitrate;
                         int newBitrate;
                         int smallestBitrate = bitrates.front();
+                        int calculatedThroughput = throughput[cur_fd] / 1.5;
 
                         for (int b : bitrates)
                         {
-                            if (b <= (throughput_map[cur_fd] / 1.5))
+                            if (b <= calculatedThroughput)
                                 newBitrate = b;
                             else
                             {
@@ -263,20 +271,16 @@ int main(int argc, char const *argv[])
                             }
                             smallestBitrate = b;
                         }
+
                         currentBitrate = newBitrate;
                         if (currentBitrate != bitrate)
                         {
                             string newChunkName = to_string(currentBitrate) + chunkName.substr(seg_index);
-                            int newChunkStartPos = request.find("GET") + 4;
-                            int newChunkEndPos = request.find(' ', newChunkStartPos);
-                            newChunkStartPos = request.rfind("vod", newChunkEndPos);
-                            request.replace(newChunkStartPos + 4, newChunkEndPos - newChunkStartPos - 4, newChunkName);
+                            request = getResponseWithNewChunkName(request, newChunkName);
                             chunkName = newChunkName;
                         }
                     }
 
-                    /* Start send request to the server */
-                    /* Create a socket */
                     int server_sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
                     if (server_sd < 0)
                     {
@@ -285,7 +289,6 @@ int main(int argc, char const *argv[])
                         return -1;
                     }
 
-                    /* Connect */
                     struct hostent *host = gethostbyname(www_ip);
                     struct sockaddr_in server_addr;
                     server_addr.sin_family = AF_INET;
@@ -310,11 +313,11 @@ int main(int argc, char const *argv[])
                     }
 
                     /* Receive response from server */
-                    string response = recv_response(server_sd);
-                    string content_type = get_value(response, "Content-Type");
-                    int content_len = atoi(get_value(response, "Content-Length").c_str());
+                    string response = receiveResponseHeader(server_sd);
+                    string contentType = getValue(response, "Content-Type");
+                    int contentLength = atoi(getValue(response, "Content-Length").c_str());
                     gettimeofday(&t1, NULL);
-                    for (int i = 0; i < content_len; i++)
+                    for (int i = 0; i < contentLength; i++)
                     {
                         if (recv(server_sd, &buf, 1, 0) < 0)
                         {
@@ -327,8 +330,8 @@ int main(int argc, char const *argv[])
                     }
                     gettimeofday(&t2, NULL);
 
-                    /* Getting video xml data and parse available bitrates */
-                    if (content_type.find("text/xml") != -1)
+                    /* Get the available bitrates */
+                    if (contentType.find("text/xml") != -1)
                     {
                         int index = 0;
                         int currentPos = response.find("bitrate=", index);
@@ -370,11 +373,11 @@ int main(int argc, char const *argv[])
                             return -1;
                         }
 
-                        response = recv_response(server_sd);
-                        content_len = atoi(get_value(response, "Content-Length").c_str());
-                        for (int i = 0; i < content_len; i++)
+                        // Receiving video xml data from server
+                        response = receiveResponseHeader(server_sd);
+                        contentLength = atoi(getValue(response, "Content-Length").c_str());
+                        for (int i = 0; i < contentLength; i++)
                         {
-                            // receiving video xml data from server
                             if (recv(server_sd, &buf, 1, 0) < 0)
                             {
                                 perror("Error recv from server");
@@ -385,25 +388,26 @@ int main(int argc, char const *argv[])
                             response += buf;
                         }
                     }
-                    /* Getting video chunk, calculate the throughput and log out info*/
-                    else if (content_type.find("video/f4f") != -1)
+
+                    // Calculate the throughput
+                    else if (contentType.find("video/f4f") != -1)
                     {
                         double duration = t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec) / 1000000.0;
-                        double t_new = (content_len / 1000) / duration * 8;
+                        double t_new = (contentLength / 1000) / duration * 8;
                         double t_cur;
-                        if (throughput_map.find(cur_fd) == throughput_map.end())
+                        if (throughput.find(cur_fd) == throughput.end())
                             t_cur = bitrates.front();
                         else
-                            t_cur = throughput_map[cur_fd];
+                            t_cur = throughput[cur_fd];
 
                         t_cur = alpha * t_new + (1 - alpha) * t_cur;
-                        throughput_map[cur_fd] = t_cur;
+                        throughput[cur_fd] = t_cur;
 
-                        log_out << client_ip << " " << chunkName << " " << string(www_ip) << " " << duration << " " << t_new << " " << t_cur << " " << currentBitrate << endl;
-                        log_out.flush();
+                        logfile << client_ip << " " << chunkName << " " << string(www_ip) << " " << duration << " " << t_new << " " << t_cur << " " << currentBitrate << endl;
+                        logfile.flush();
                     }
 
-                    /* Send response to client */
+                    // Send response to client 
                     if (send(cur_fd, response.c_str(), response.size(), 0) < 0)
                     {
                         perror("Error sending to client");
@@ -415,7 +419,7 @@ int main(int argc, char const *argv[])
             }
         }
     }
-    log_out.close();
+    logfile.close();
     close(socket_fd);
     return 0;
 }
